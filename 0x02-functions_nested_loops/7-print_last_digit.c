02822c0938583c4ffa22e4e651ce69c56aa199be
Ibrahim Akande <ibrahimakande94@yahoo.com>
2023-03-21 20:38:34 -0700

    Created 7-print_last_digit.c file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * print_last_digit - prints the res digit of a number.
+ * @n: the input number.
+ * Return:res digit of a number.
+ */
+int print_last_digit(int n)
+{
+long int m = n;
+int res;
+if (m < 0)
+m = -m;
+res = m % 10;
+_putchar(res + '0');
+return (res);
+}
