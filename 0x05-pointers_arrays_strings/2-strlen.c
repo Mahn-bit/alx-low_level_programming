554ab188c8e576db19f32d29015719586e71cdb6
Ibrahim Akande <ibrahimakande94@yahoo.com>
2023-03-27 21:42:10 -0700

    Created 2-strien.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * _strlen - This function returns the length of a string.
+ * @str: The string to get the length
+ * Return: The length of @str.
+ */
+
+int _strlen(char *str)
+
+{
+
+	size_t length = 0;
+
+	while (*str++)
+	length++;
+	return (length);
+
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
